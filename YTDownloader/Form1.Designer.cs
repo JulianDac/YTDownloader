119f92a02cb2e61146e94827bb44fabd0555adc6
Julian Dac <s3748402@student.rmit.edu.au>
2020-08-06 14:53:12 +1000

    initial commit can recognize the video's ID, size, resolution and extension

--- YTDownloader/Form1.Designer.cs
+++ YTDownloader/Form1.Designer.cs
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.label1 = new System.Windows.Forms.Label();
+            this.textURL = new System.Windows.Forms.TextBox();
+            this.Btnget = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.Btndownload = new System.Windows.Forms.Button();
+            this.ProgressBarDownload = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Video URL:";
+            // 
+            // textURL
+            // 
+            this.textURL.Location = new System.Drawing.Point(166, 88);
+            this.textURL.Name = "textURL";
+            this.textURL.Size = new System.Drawing.Size(519, 23);
+            this.textURL.TabIndex = 1;
+            // 
+            // Btnget
+            // 
+            this.Btnget.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Btnget.Location = new System.Drawing.Point(166, 117);
+            this.Btnget.Name = "Btnget";
+            this.Btnget.Size = new System.Drawing.Size(171, 51);
+            this.Btnget.TabIndex = 2;
+            this.Btnget.Text = "Get Video";
+            this.Btnget.UseVisualStyleBackColor = true;
+            this.Btnget.Click += new System.EventHandler(this.Btnget_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(166, 174);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(519, 117);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Type";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Quality";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Size";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Extension";
+            this.columnHeader4.Width = 250;
+            // 
+            // Btndownload
+            // 
+            this.Btndownload.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Btndownload.Location = new System.Drawing.Point(166, 297);
+            this.Btndownload.Name = "Btndownload";
+            this.Btndownload.Size = new System.Drawing.Size(118, 37);
+            this.Btndownload.TabIndex = 2;
+            this.Btndownload.Text = "Download";
+            this.Btndownload.UseVisualStyleBackColor = true;
+            // 
+            // ProgressBarDownload
+            // 
+            this.ProgressBarDownload.Location = new System.Drawing.Point(166, 354);
+            this.ProgressBarDownload.Name = "ProgressBarDownload";
+            this.ProgressBarDownload.Size = new System.Drawing.Size(519, 23);
+            this.ProgressBarDownload.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(781, 498);
+            this.Controls.Add(this.ProgressBarDownload);
+            this.Controls.Add(this.Btndownload);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.Btnget);
+            this.Controls.Add(this.textURL);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Youtube Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
+
+
+
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textURL;
+        private System.Windows.Forms.Button Btnget;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button Btndownload;
+        private System.Windows.Forms.ProgressBar ProgressBarDownload;
     }
 }
 
