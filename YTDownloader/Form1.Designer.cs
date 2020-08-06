6309509182632ecac25b389af264a3e3747f345d
Julian Dac <s3748402@student.rmit.edu.au>
2020-08-07 00:23:30 +1000

    All features work. Added loading gif.

--- YTDownloader/Form1.Designer.cs
+++ YTDownloader/Form1.Designer.cs
@@ -6,6 +6,7 @@
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
 
         /// <summary>
         ///  Clean up any resources being used.
@@ -28,7 +29,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.VideoURLLabel = new System.Windows.Forms.Label();
             this.textURL = new System.Windows.Forms.TextBox();
             this.Btnget = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
@@ -37,22 +39,26 @@
             this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
             this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
             this.Btndownload = new System.Windows.Forms.Button();
-            this.ProgressBarDownload = new System.Windows.Forms.ProgressBar();
+            this.progressBarDownload = new System.Windows.Forms.ProgressBar();
             this.TextVideoName = new System.Windows.Forms.TextBox();
             this.VideoNameLabel = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.ThumbnailLabel = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBoxLoadGet = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLoadGet)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // VideoURLLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 81);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Video URL:";
+            this.VideoURLLabel.AutoSize = true;
+            this.VideoURLLabel.Location = new System.Drawing.Point(58, 81);
+            this.VideoURLLabel.Name = "VideoURLLabel";
+            this.VideoURLLabel.Size = new System.Drawing.Size(64, 15);
+            this.VideoURLLabel.TabIndex = 0;
+            this.VideoURLLabel.Text = "Video URL:";
             // 
             // textURL
             // 
@@ -79,6 +85,7 @@
             this.columnHeader2,
             this.columnHeader3,
             this.columnHeader4});
+            this.listView1.FullRowSelect = true;
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(150, 164);
             this.listView1.Name = "listView1";
@@ -111,19 +118,20 @@
             // Btndownload
             // 
             this.Btndownload.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Btndownload.Location = new System.Drawing.Point(150, 373);
+            this.Btndownload.Location = new System.Drawing.Point(504, 324);
             this.Btndownload.Name = "Btndownload";
-            this.Btndownload.Size = new System.Drawing.Size(118, 37);
+            this.Btndownload.Size = new System.Drawing.Size(165, 88);
             this.Btndownload.TabIndex = 2;
             this.Btndownload.Text = "Download";
             this.Btndownload.UseVisualStyleBackColor = true;
+            this.Btndownload.Click += new System.EventHandler(this.Btndownload_Click);
             // 
-            // ProgressBarDownload
+            // progressBarDownload
             // 
-            this.ProgressBarDownload.Location = new System.Drawing.Point(150, 426);
-            this.ProgressBarDownload.Name = "ProgressBarDownload";
-            this.ProgressBarDownload.Size = new System.Drawing.Size(519, 23);
-            this.ProgressBarDownload.TabIndex = 4;
+            this.progressBarDownload.Location = new System.Drawing.Point(150, 426);
+            this.progressBarDownload.Name = "progressBarDownload";
+            this.progressBarDownload.Size = new System.Drawing.Size(519, 23);
+            this.progressBarDownload.TabIndex = 4;
             // 
             // TextVideoName
             // 
@@ -141,39 +149,69 @@
             this.VideoNameLabel.TabIndex = 0;
             this.VideoNameLabel.Text = "Video Name:";
             // 
-            // label2
+            // ThumbnailLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(354, 332);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 15);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Thumbnail:";
+            this.ThumbnailLabel.AutoSize = true;
+            this.ThumbnailLabel.Location = new System.Drawing.Point(58, 324);
+            this.ThumbnailLabel.Name = "ThumbnailLabel";
+            this.ThumbnailLabel.Size = new System.Drawing.Size(67, 15);
+            this.ThumbnailLabel.TabIndex = 0;
+            this.ThumbnailLabel.Text = "Thumbnail:";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(427, 332);
+            this.pictureBox1.Location = new System.Drawing.Point(150, 324);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(242, 88);
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(150, 456);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(23, 15);
+            this.labelProgress.TabIndex = 7;
+            this.labelProgress.Text = "0 %";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = false;
+            this.backgroundWorker1.WorkerSupportsCancellation = false;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // pictureBoxLoadGet
+            // 
+            this.pictureBoxLoadGet.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLoadGet.Image")));
+            this.pictureBoxLoadGet.Location = new System.Drawing.Point(327, 108);
+            this.pictureBoxLoadGet.Name = "pictureBoxLoadGet";
+            this.pictureBoxLoadGet.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxLoadGet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLoadGet.TabIndex = 8;
+            this.pictureBoxLoadGet.TabStop = false;
+            this.pictureBoxLoadGet.Visible = false;
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(726, 498);
+            this.Controls.Add(this.pictureBoxLoadGet);
+            this.Controls.Add(this.labelProgress);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ThumbnailLabel);
             this.Controls.Add(this.VideoNameLabel);
             this.Controls.Add(this.TextVideoName);
-            this.Controls.Add(this.ProgressBarDownload);
+            this.Controls.Add(this.progressBarDownload);
             this.Controls.Add(this.Btndownload);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.Btnget);
             this.Controls.Add(this.textURL);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.VideoURLLabel);
             this.Name = "Form1";
             this.Text = "Youtube Downloader";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLoadGet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,7 +222,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label VideoURLLabel;
         private System.Windows.Forms.TextBox textURL;
         private System.Windows.Forms.Button Btnget;
         private System.Windows.Forms.ListView listView1;
@@ -193,11 +231,13 @@
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader4;
         private System.Windows.Forms.Button Btndownload;
-        private System.Windows.Forms.ProgressBar ProgressBarDownload;
+        private System.Windows.Forms.ProgressBar progressBarDownload;
         private System.Windows.Forms.TextBox TextVideoName;
         private System.Windows.Forms.Label VideoNameLabel;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ThumbnailLabel;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.PictureBox pictureBoxLoadGet;
     }
 }
 
